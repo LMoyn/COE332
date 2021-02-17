678845f486827accbeb76c05b9d569cfee7341f8
Lucas Moynihan <lucasmoynihan1@gmail.com>
2021-02-17 01:19:59 -0600

    Fixed generation

--- homework02/generate_animals.py
+++ homework02/generate_animals.py
@@ -7,17 +7,20 @@ import sys
 #Each dictionary describes a single animal
 animals = [dict() for x in range(20)]
 
+#Gives head choices
+heads = ['snake', 'bull', 'lion', 'raven', 'bunny']
+
 #Loops over the 20 animals we generate
 iterator = 0
 while iterator < 20:
-    animals[iterator]['head'] = petname.name()
+    animals[iterator]['head'] = petname.random.choice(heads)
     #Generates two animal names and concatenates them to make the body
     body1 = petname.name()
     body2 = petname.name()
     body = body1 + "-"+body2
     animals[iterator]['body'] = body
-    animals[iterator]['arms'] = petname.random.randint(1,6)
-    animals[iterator]['legs'] = petname.random.randint(2,12)
+    animals[iterator]['arms'] = 2*petname.random.randint(1,5)
+    animals[iterator]['legs'] = 3*petname.random.randint(1,4)
     animals[iterator]['tail'] = animals[iterator]['arms']+animals[iterator]['legs']
     iterator = iterator +1
 
