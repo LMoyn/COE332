748098dede3fb04bb6c70be39629ba8878200bac
Lucas Moynihan <lucasmoynihan1@gmail.com>
2021-02-20 00:07:21 -0600

    Fixed unit test

--- homework02/read_animals.py
+++ homework02/read_animals.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 import json
-import random
+import petname
 import sys
 
 #Gives an animal resulting from the mixing of two input animals' traits
-def breedAnimals(firstParentIndex, secondParentIndex, animals):
-    offspring = dict()
+def breedAnimals(firstParentIndex, secondParentIndex, animals_dict):
+    offspring = {}
+    
+
     #Randomly selects to have one of the parents' body's and head
-    offspring['head'] = petname.random.choice([animals[firstParentIndex]['head'], animals[secondParentIndex]['head']])
+    offspring['head'] = petname.random.choice([ animals_dict[firstParentIndex]['head'], animals_dict[secondParentIndex]['head'] ])
     
     #Creates a list containing both animals making up each body
-    parent1_body_animals = (animals[firstParentIndex]['body']).split("-")
-    parent2_body_animals = (animals[secondParentIndex]['body']).split("-")
+    parent1_body_animals = (animals_dict[firstParentIndex]['body']).split("-")
+    parent2_body_animals = (animals_dict[secondParentIndex]['body']).split("-")
 
     body1 = petname.random.choice([parent1_body_animals[0], parent1_body_animals[1]])
     body2 = petname.random.choice([parent2_body_animals[0], parent2_body_animals[1]])
@@ -19,8 +21,8 @@ def breedAnimals(firstParentIndex, secondParentIndex, animals):
     offspring['body'] = offspring_body
     
     #Averages the number of arms and legs of the parents to get offspring data
-    offspring['arms'] = round((animals[firstParentIndex]['arms'] + animals[secondParentIndex]['arms'] )/2)
-    offspring['legs'] = round((animals[firstParentIndex]['legs'] + animals[secondParentIndex]['legs'] )/2)
+    offspring['arms'] = round((animals_dict[firstParentIndex]['arms'] + animals_dict[secondParentIndex]['arms'] )/2)
+    offspring['legs'] = round((animals_dict[firstParentIndex]['legs'] + animals_dict[secondParentIndex]['legs'] )/2)
     offspring['tail'] = offspring['arms'] + offspring['legs']
     return offspring
 
@@ -29,7 +31,12 @@ def main():
     with open(sys.argv[1], 'r') as f:
         animals = json.load(f)
 
-    print(random.choice(animals['animals']))
+    animal_dict = animals['animals']
+    print(petname.random.choice(animal_dict))
+    parent1 = petname.random.randint(0,19)
+    parent2 = petname.random.randint(0,19)
+    child = breedAnimals(parent1, parent2, animal_dict)
+    print(child)
 
 if __name__ == '__main__':
     main()
--- homework02/test_read_animals.py
+++ homework02/test_read_animals.py
@@ -1,58 +1,69 @@
 #!/usr/bin/env python3
 import unittest
-import json
 import sys
+import petname
 from read_animals import breedAnimals
 
 class Test_read_animals(unittest.TestCase):
-
+   
     def test_breedAnimals(self):
-        #Opens the json file
-        with open(sys.argv[1], 'r') as f:
-        	animals = json.load(f)
+
+        #Generates a set of animals to test over
+        animal_dict = {}
+        animal_dict['animals'] = []
+        
+        for i in range(20):
+            this_animal = {}
+            this_animal['head'] = petname.random.choice(['snake', 'bull', 'lion', 'raven', 'bunny'])
+            this_animal['body'] = petname.name() + '-' + petname.name()
+            this_animal['arms'] = petname.random.randint(1,5) * 2
+            this_animal['legs'] = petname.random.randint(1,4) * 3
+            this_animal['tail'] = this_animal['legs'] + this_animal['arms']
+            animal_dict['animals'].append(this_animal)
+        
+        animal_list = animal_dict['animals']
 
         #The codes tests over parent pairs with the (x,y) pairs below as their indices
-        xlist = [1,3,7,10,19]
-        ylist = [7,18,5,13,9]
-        iterator = 0
-        while iterator < len(xlist):
+        x = 0
+        y= 0
+        while x < 20:
+            y = 0
+            while y < 20:
             #Performs checks for the offspring of a the xth and yth animals
-            x = xlist[iterator]
-            y = ylist[iterator]
+            
+                parent1 = animal_list[x]
+                parent2 = animal_list[y]
+                offspring = breedAnimals(x,y,animal_list)
 
-            parent1 = animals[x]
-            parent2 = animals[y]
-            offspring = breedAnimals(x,y,animals)
+                #Checks that the head is the same as the head of one of the parents
 
-            #Checks that the head is the same as the head of one of the parents
+                parent1_in_head = (parent1['head']) in offspring['head']
+                parent2_in_head = parent2['head'] in offspring['head']
+                self.assertTrue(parent1_in_head or parent2_in_head)
 
-            parent1_in_head = (parent1['head']) in offspring['head']
-            parent2_in_head = parent2['head'] in offspring['head']
-            self.assertTrue(parent1_in_head or parent2_in_head)
+                #Creates a list containing both animals making up each body
+                parent1_body_animals = parent1['body'].split("-")
+                parent2_body_animals = parent2['body'].split("-")
 
-            #Creates a list containing both animals making up each body
-            parent1_body_animals = parent1['body'].split("-")
-            parent2_body_animals = parent2['body'].split("-")
+                #Checks that at least one animal from each of the parents' bodies
+                #Are present in the offspring body
+                parent1_in_offspring_body = parent1_body_animals[0] in offspring['body'] or parent1_body_animals[1] in offspring['body']
+                parent2_in_offspring_body = parent2_body_animals[0] in offspring['body'] or parent2_body_animals[1] in offspring['body']
+                self.assertTrue(parent1_in_offspring_body and parent2_in_offspring_body)
 
-            #Checks that at least one animal from each of the parents' bodies
-            #Are present in the offspring body
-            parent1_in_offspring_body = parent1_body_animals[0] in offspring['body'] or parent1_body_animals[1] in offspring['body']
-            parent2_in_offspring_body = parent2_body_animals[0] in offspring['body'] or parent2_body_animals[1] in offspring['body']
-            self.assertTrue(parent1_in_offspring_body and parent2_in_offspring_body)
+                #Checks that the number of arms matches what we get from averaging the values from the parents
+                average_arms = round((parent1['arms'] + parent2['arms'] )/2)
+                self.assertEqual(average_arms,offspring['arms'])
 
-            #Checks that the number of arms matches what we get from averaging the values from the parents
-            average_arms = round((parent1['arms'] + parent2['arms'] )/2)
-            self.assertEqual(average_arms,offspring['arms'])
+                #Checks that the number of legs matches what we get from averaging the values from the parents
+                average_legs = round((parent1['legs'] + parent2['legs'] )/2)
+                self.assertEqual(average_legs,offspring['legs'])
 
-            #Checks that the number of legs matches what we get from averaging the values from the parents
-            average_legs = round((parent1['legs'] + parent2['legs'] )/2)
-            self.assertEqual(average_legs,offspring['legs'])
+                #Checks that the tail is what we expect from the arm and leg values
+                self.assertEqual(average_arms+average_legs, offspring['tail'])
 
-            #Checks that the tail is what we expect from the arm and leg values
-            self.assertEqual(average_arms+average_legs, offspring['tail'])
-
-            iterator = iterator + 1
+                y = y+1
+            x = x+1
 
 if __name__ == '__main__':
-	with open(sys.argv[1], 'r') as f:                                                                                                                                    animals = json.load(f)   
-	unittest.main()
+    unittest.main()
